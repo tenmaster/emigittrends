6a8abcb020f1373983093363d8ba78b1e3271ceb
Emiliano <tenlion@hotmail.com>
2018-02-23 02:46:20 -0300

    Add response listener on the activity

--- app/src/main/java/com/emigittrends/activities/MainActivity.java
+++ app/src/main/java/com/emigittrends/activities/MainActivity.java
@@ -2,17 +2,32 @@ package com.emigittrends.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.emigittrends.R;
+import com.emigittrends.model.GitRepository;
+import com.emigittrends.model.HotRespositoriesResponse;
 import com.emigittrends.network.EmiHttpClient;
+import com.emigittrends.network.EmiHttpClient.HttpClientListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements HttpClientListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EmiHttpClient.getInstance().getHotRepositories(this);
+    }
 
-        EmiHttpClient.getInstance().getHotRepositories();
+    @Override
+    public void onRepositoriesRequestSuccess(ArrayList<GitRepository> repositories) {
+        
+    }
+
+    @Override
+    public void onRepositoriesRequestFailed() {
+        Toast.makeText(this, getString(R.string.error_loading_repositories), Toast.LENGTH_SHORT).show();
     }
 }
