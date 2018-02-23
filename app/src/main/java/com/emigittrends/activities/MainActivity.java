90b7fd518a9286f195e547abc6cb3e58cb8edfe1
Emiliano <tenlion@hotmail.com>
2018-02-23 03:40:33 -0300

    Setup adapter
    And finally show data!

--- app/src/main/java/com/emigittrends/activities/MainActivity.java
+++ app/src/main/java/com/emigittrends/activities/MainActivity.java
@@ -2,9 +2,12 @@ package com.emigittrends.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import com.emigittrends.R;
+import com.emigittrends.adapters.MainRepositoriesAdapter;
 import com.emigittrends.model.GitRepository;
 import com.emigittrends.network.EmiHttpClient;
 import com.emigittrends.network.EmiHttpClient.HttpClientListener;
@@ -13,16 +16,26 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements HttpClientListener{
 
+    RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupAdapter();
+
         EmiHttpClient.getInstance().getHotRepositories(this);
+    }
+
+    private void setupAdapter() {
+        mRecyclerView = findViewById(R.id.main_activity_recycler_view);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
     }
 
     @Override
     public void onRepositoriesRequestSuccess(ArrayList<GitRepository> repositories) {
-
+        mRecyclerView.setAdapter(new MainRepositoriesAdapter(repositories));
     }
 
     @Override
